aa93754e212b4a82db1c13de13a2bd4781d08537
KudriavtsevaEkaterina235 <kudriavtseva.ekaterina007@gmail.com>
2026-09-09 10:13:48 +0300

    Add files via upload

---
+++ LESSON5/src/Main.java
@@ -0,0 +1,28 @@
+import WorkCalculator.WorkCalculator;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя работника:");
+        WorkCalculator calculator = new WorkCalculator(scanner.next());
+
+        System.out.println("Введите коэффициент в формате 1.5:");
+        calculator.coefficient = scanner.nextDouble();
+
+        System.out.println("Введите количество отработанных дней без коэффициента:");
+        double hoursWithoutCoefficient = calculator.calculate(scanner.nextInt());
+
+        System.out.println("Введите количество отработанных дней с коэффициентом:");
+        double hoursWithCoefficient = calculator.calculateWithCoefficient(scanner.nextInt());
+
+        double allWorkHours = hoursWithoutCoefficient + hoursWithCoefficient;
+        // метод printf позволяет нам вывести строку с форматированием
+        // символ %s заменит строку, а %.2f число с 2 знаками после запятой
+        System.out.printf("Работник %s отработал %.2f часов.",
+                calculator.getWorkerName(), allWorkHours);
+    }
+}
